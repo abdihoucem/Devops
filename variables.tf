239cc8fb20c411cf9d346c2195dcabdd85e07784
abdihoucem <abdihoucemeddine63@gmail.com>
2022-03-03 17:45:18 +0100

    test

--- variables.tf
+++ variables.tf
@@ -120,7 +120,7 @@ variable "private_DB_subnet_2a_cidr" {
 variable "private_DB_subnet_2b_cidr" {
     description = "CIDR for the Private 2b Subnet"
     default = "10.0.2.128/25"  
-}
+}test
 
 
 
